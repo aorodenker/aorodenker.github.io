10c50232897bfa00a8a8811a25e99768ab2b7a31
Andrew <andrew.orodenker@gmail.com>
2022-11-08 14:01:48 -0800

    cleaned unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-// import AboutMe from './components/AboutMe';
-// import Education from './components/Education';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import Projects from './components/Projects';
 import Content from './components/Content';
 import ParticleAnimation from './components/ParticleAnimation';
 
